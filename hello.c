b910ac909d08d2f06d473bf36923c78f8a273221
Andy <andik8086@gmail.com>
2026-03-19 13:14:23 +0300

    My first change after push

--- hello.c
+++ hello.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int factorial(int n){
+ if (n==1) return 1;
+
+ return n*factorial(n-1);
+}
+
 void main()
 {
- printf("Hi\n");
-
+ printf("Hi\n\n");
+ printf("Let me change something...\n\n");
+ printf("I calculated fac(4) = %d\n",factorial(4));
 }
+
